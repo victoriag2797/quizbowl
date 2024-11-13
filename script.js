e028599512e6179383e5220860830e886dd73eb6
victoriag2797 <victoriag2797@tuckahoeschools.org>
2024-11-13 13:31:33 +0000

    giving up

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 
 
-const changeText = document.querySelector("#change-text");
+const score = document.querySelector('.score');
+const submitButton = document.querySelector('.submit-button');
+const input = document.querySelector('.input-field');
+const header = document.querySelector('.header');
+const answerList = ["Patrick Kane", "The Chicago Blackhawks", "Nico Hischier", "Erica Colabella", "Tim Lincecum"]
 
-changeText.addEventListener("click", function() {
-  changeText.textContent = "Text has been changed!";
+let questionList = 
+
+let score = 0;
+let answerList = ['1', '2', '3'];
+const question = document.querySelector('.questioncontainer');
+question.textContent = answer list[0;]
+
+// Who is Showtime?
+// What team won the Stanley Cup in 2013?
+// Who is the princess of Switzerland?
+// Who is the curse of the Yankees?
+// Who is the Freak (Giants)?
