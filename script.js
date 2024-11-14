ec3df58b1522b7e6c51e118e8ce61a46fc7bcf0e
victoriag2797 <victoriag2797@tuckahoeschools.org>
2024-11-14 13:32:37 +0000

    commit

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 
 
-const score = document.querySelector('.score');
 const submitButton = document.querySelector('.submit-button');
-const input = document.querySelector('.input-field');
-const header = document.querySelector('.header');
-const answerList = ["Patrick Kane", "The Chicago Blackhawks", "Nico Hischier", "Erica Colabella", "Tim Lincecum"]
+const input = document.querySelector('#input-container');
+const question = document.querySelector('.question-container');
 
-let questionList = 
+let scoreContainer = document.querySelector('.score-container')
+
+let questionIndex = 0;
+
+let answerList = ["Patrick Kane", "The Chicago Blackhawks", "Nico Hischier", "Erica Colabella", "Tim Lincecum", "Sidney Crosby"];
+
+let questionList = ["Who is Showtime?", "What team won the Stanley Cup in 2013?", 
+    "Who is the princess of Switzerland?", 
+    "Who is the curse of the Yankees?",
+    "Who is the Freak (Giants)?",
+    "Who is the destined goalie of the Pittsburgh Penguins?"
+];
 
 let score = 0;
-let answerList = ['1', '2', '3'];
-const question = document.querySelector('.questioncontainer');
-question.textContent = answer list[0;]
 
-// Who is Showtime?
-// What team won the Stanley Cup in 2013?
-// Who is the princess of Switzerland?
-// Who is the curse of the Yankees?
-// Who is the Freak (Giants)?
+let submitFunction = () =>{
+    submitButton.addEventListener('click', () =>{
+        if(input.value === answerList[questionIndex]){
+            questionIndex += 1;
+            score += 1;
+            question.textContent = questionList[questionIndex];
+            scoreContainer.textContent = "Score: " + score;
+        }else {
+            score -= 1;
+            scoreContainer.textContent = "Score: " + score;
+        }
+        /// put endgame function here 
+    });
+}
+
+question.textContent = questionList[questionIndex];
+
+let endGame = () => {
+    if(questionIndex > 5){
+        scoreContainer.textContent = "Final Score:"
+    }
+}
